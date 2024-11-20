72880ce2b47379057daa8958fbc2a66b2bc8c973
Telas <6520150012@utch.edu.mx>
2024-11-19 18:47:27 -0600

    Arrelar

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 if ('serviceWorker' in navigator) {
   console.log('Si tiene Service Worker');
 
-  navigator.serviceWorker.register('/PWA2024/sw.js')  // Asegúrate de que la ruta sea correcta
+  navigator.serviceWorker.register('./assets/js/sw.js')  // Asegúrate de que la ruta sea correcta
       .then(res => console.log('Service Worker cargando correctamente', res))
       .catch(err => console.log('Service Worker no se pudo cargar', err));  // Cambiado 'res' por 'err'
 } else {
   console.log('No se localiza Service Worker');
-}
-
-if ("serviceWorker" in navigator) {
-  console.log("si tiene");
-
-  navigator.serviceWorker
-    .register("/PWA2024/sw.js")
-    .then((res) => console.log("serviceWorker cargado"))
-    .catch((err) => console.log("serviceWorker no se puede cargar"));
-} else {
-  console.log("no se enceuntra");
 }
